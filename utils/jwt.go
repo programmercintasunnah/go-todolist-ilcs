b5f4c96953a73001e60d3be53f68d09cf7c829bd
programmercintasunnah <muhammadzakie22g@gmail.com>
2024-11-10 15:01:54 +0700

    utils

---
+++ utils/jwt.go
@@ -0,0 +1,50 @@
+// utils/jwt.go
+package utils
+
+import (
+	"errors"
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
+)
+
+type Claims struct {
+	Username string `json:"username"`
+	jwt.StandardClaims
+}
+
+func GenerateJWT(username string, duration time.Duration) (string, error) {
+	expirationTime := time.Now().Add(duration)
+	claims := &Claims{
+		Username: username,
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: expirationTime.Unix(),
+			IssuedAt:  time.Now().Unix(),
+		},
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	tokenString, err := token.SignedString([]byte("your-secret-key")) // Ganti dengan secret dari config
+	if err != nil {
+		return "", err
+	}
+
+	return tokenString, nil
+}
+
+func ParseJWT(tokenStr string, secret string) (*Claims, error) {
+	claims := &Claims{}
+
+	token, err := jwt.ParseWithClaims(tokenStr, claims, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, errors.New("unexpected signing method")
+		}
+		return []byte(secret), nil
+	})
+
+	if err != nil || !token.Valid {
+		return nil, errors.New("invalid token")
+	}
+
+	return claims, nil
+}
---
+++ utils/validator.go
@@ -0,0 +1,12 @@
+// utils/validator.go
+package utils
+
+import (
+	"github.com/go-playground/validator/v10"
+)
+
+var Validate *validator.Validate
+
+func init() {
+	Validate = validator.New()
+}
