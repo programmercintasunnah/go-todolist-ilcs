b03e1878f1ad0bbe200e8299706f1482576e841f
programmercintasunnah <muhammadzakie22g@gmail.com>
2024-11-10 14:24:28 +0700

    go todo-list ilcs (10nov2024 14:24wib)

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # go-todolist-ilcs
-# go-todolist-ilcs
---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+func main() {
+	// todo-api/
+	// ├── cmd/
+	// │   └── main.go
+	// ├── config/
+	// │   └── config.go
+	// ├── controllers/
+	// │   └── task_controller.go
+	// ├── models/
+	// │   └── task.go
+	// ├── repositories/
+	// │   └── task_repository.go
+	// ├── services/
+	// │   └── task_service.go
+	// ├── middlewares/
+	// │   └── auth.go
+	// │   └── logger.go
+	// ├── utils/
+	// │   └── jwt.go
+	// │   └── validator.go
+	// ├── tests/
+	// │   └── task_test.go
+	// ├── go.mod
+	// ├── go.sum
+
+	// mkdir todo-api
+	// cd todo-api
+	// go mod init github.com/username/todo-api
+
+	// go get -u github.com/gin-gonic/gin
+	// go get -u gorm.io/gorm
+	// go get -u gorm.io/driver/oracle
+	// go get -u github.com/go-redis/redis/v8
+	// go get -u github.com/dgrijalva/jwt-go
+	// go get -u github.com/go-playground/validator/v10
+	// go get -u github.com/sirupsen/logrus
+	// go get -u github.com/stretchr/testify
+}
